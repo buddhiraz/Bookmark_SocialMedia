ae0d128cc1bb45adb2661c9abdd753fbb5faa1ec
BuddhirajSahu <47522091+buddhiraz@users.noreply.github.com>
2024-02-02 13:52:18 +0530

    Update wsgi.py for vercel

--- bookmarks/wsgi.py
+++ bookmarks/wsgi.py
@@ -13,4 +13,5 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bookmarks.settings')
 
-application = get_wsgi_application()
+# vercel_app/wsgi.py
+app = get_wsgi_application()
